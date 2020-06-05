7053a0b5d5f8c810171b49f323917e46b983569e
Stephane CRIVISIER <stephane@crivisier.fr>
2020-06-05 14:42:16 +0200

    Remove JSON format from the admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,13 +3,11 @@ module Admin
     before_action :set_product, only: [:show, :edit, :update, :destroy]
 
     # GET /products
-    # GET /products.json
     def index
       @products = Product.all
     end
 
     # GET /products/1
-    # GET /products/1.json
     def show
     end
 
@@ -23,54 +21,42 @@ module Admin
     end
 
     # POST /products
-    # POST /products.json
     def create
       @product = Product.new(product_params)
 
-      respond_to do |format|
-        if @product.save
-          format.html { redirect_to [:admin, @product], notice: 'Product was successfully created.' }
-          format.json { render :show, status: :created, location: @product }
-        else
-          format.html { render :new }
-          format.json { render json: @product.errors, status: :unprocessable_entity }
-        end
+      if @product.save
+        redirect_to [:admin, @product], notice: 'Product was successfully created.'
+      else
+        render :new
       end
     end
 
     # PATCH/PUT /products/1
-    # PATCH/PUT /products/1.json
     def update
-      respond_to do |format|
-        if @product.update(product_params)
-          format.html { redirect_to [:admin, @product], notice: 'Product was successfully updated.' }
-          format.json { render :show, status: :ok, location: @product }
-        else
-          format.html { render :edit }
-          format.json { render json: @product.errors, status: :unprocessable_entity }
-        end
+      if @product.update(product_params)
+        redirect_to [:admin, @product], notice: 'Product was successfully updated.'
+      else
+        render :edit
       end
     end
 
     # DELETE /products/1
-    # DELETE /products/1.json
     def destroy
       @product.destroy
-      respond_to do |format|
-        format.html { redirect_to admin_products_url, notice: 'Product was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+
+      redirect_to admin_products_url, notice: 'Product was successfully destroyed.'
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_product
-        @product = Product.find(params[:id])
-      end
 
-      # Only allow a list of trusted parameters through.
-      def product_params
-        params.require(:product).permit(:name, :description, :price, :shop_id)
-      end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :shop_id)
+    end
   end
 end
--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -3,13 +3,11 @@ module Admin
     before_action :set_shop, only: [:show, :edit, :update, :destroy]
 
     # GET /shops
-    # GET /shops.json
     def index
       @shops = Shop.all
     end
 
     # GET /shops/1
-    # GET /shops/1.json
     def show
     end
 
@@ -23,54 +21,42 @@ module Admin
     end
 
     # POST /shops
-    # POST /shops.json
     def create
       @shop = Shop.new(shop_params)
 
-      respond_to do |format|
-        if @shop.save
-          format.html { redirect_to [:admin, @shop], notice: 'Shop was successfully created.' }
-          format.json { render :show, status: :created, location: @shop }
-        else
-          format.html { render :new }
-          format.json { render json: @shop.errors, status: :unprocessable_entity }
-        end
+      if @shop.save
+        redirect_to [:admin, @shop], notice: 'Shop was successfully created.'
+      else
+        render :new
       end
     end
 
     # PATCH/PUT /shops/1
-    # PATCH/PUT /shops/1.json
     def update
-      respond_to do |format|
-        if @shop.update(shop_params)
-          format.html { redirect_to [:admin, @shop], notice: 'Shop was successfully updated.' }
-          format.json { render :show, status: :ok, location: @shop }
-        else
-          format.html { render :edit }
-          format.json { render json: @shop.errors, status: :unprocessable_entity }
-        end
+      if @shop.update(shop_params)
+        redirect_to [:admin, @shop], notice: 'Shop was successfully updated.'
+      else
+        render :edit
       end
     end
 
     # DELETE /shops/1
-    # DELETE /shops/1.json
     def destroy
       @shop.destroy
-      respond_to do |format|
-        format.html { redirect_to admin_shops_url, notice: 'Shop was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+
+      redirect_to admin_shops_url, notice: 'Shop was successfully destroyed.'
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_shop
-        @shop = Shop.find(params[:id])
-      end
 
-      # Only allow a list of trusted parameters through.
-      def shop_params
-        params.require(:shop).permit(:name, :slug, :available_on)
-      end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_shop
+      @shop = Shop.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def shop_params
+      params.require(:shop).permit(:name, :slug, :available_on)
+    end
   end
 end
